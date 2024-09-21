a891353042280dfcd0bd1aa27994ac46a4d004d3
Aysenur Coskuner <aysenurkubrasurucu@gmail.com>
2024-09-21 22:47:13 +0000

    bug fix

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,26 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const msgs = document.getElementById('msgs');
+    var msgs = document.getElementById('msgs');
     if (msgs) {
-        setTimeout(() => {
-            const alerts = msgs.querySelectorAll('.alert');
-            alerts.forEach(alert => {
+        setTimeout(function() {
+            var alerts = msgs.querySelectorAll('.alert');
+            alerts.forEach(function(alert) {
                 alert.style.transition = 'opacity 3s';
                 alert.style.opacity = '0';
             });
         }, 5000);
 
-        setTimeout(() => {
+        setTimeout(function() {
             msgs.remove();
         }, 8000);
     }
-});
-
-const dropdowns = document.querySelectorAll('.dropdown');
-dropdowns.forEach(dropdown => {
-    dropdown.addEventListener('mouseover', function() {
-        this.querySelector('.dropdown-menu').classList.add('show');
-    });
-    dropdown.addEventListener('mouseout', function() {
-        this.querySelector('.dropdown-menu').classList.remove('show');
-    });
 });
