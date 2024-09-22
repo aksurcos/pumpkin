7083b55889509771e80bbc13b8ab65d2f3acf8d3
Aysenur Coskuner <aysenurkubrasurucu@gmail.com>
2024-09-22 04:48:09 +0000

    deploy bug

--- story/urls.py
+++ story/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.storyList, name='story'),
-     path('filter/', views.story_list, name='story_list'),
-    path('create', views.story_create, name='story_create'),
-    path('edit/<int:id>', views.edit, name='edit_story'),
-    path('delete/<int:id>', views.delete, name='delete_story'),
+    path('filter/', views.story_list, name='story_list'),
+    path('create/', views.story_create, name='story_create'),
+    path('edit/<int:id>/', views.edit, name='edit_story'),
+    path('delete/<int:id>/', views.delete, name='delete_story'),
     path('<slug:slug>/', views.story_details, name='story_details'),
     path('<slug:slug>/comment/', views.add_comment, name='add_comment'),
 ]
